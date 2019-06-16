c8a748ad9def6bde2da64dcc898df87e80857741
Robin Wieruch <wrobin@gmx.net>
2019-06-16 18:22:25 +0200

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  // 1
+  entry: './src/index.js',
+  // 2
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  // 3
+  devServer: {
+    contentBase: './dist'
+  }
+};
